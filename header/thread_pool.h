e34f343bcd5953655a503f162e058c40fe2cd49e
qcezads123-YC <qcezads123456@gmail.com>
2026-05-10 00:16:39 +0800

    initial commit

---
+++ header/thread_pool.h
@@ -0,0 +1,8 @@
+#ifndef TPOOL_H
+#define TPOOL_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#endif
+void *worker(void *arg);
---
+++ thread_pool.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include "thread_pool.h"
+typedef struct thread_pool_t
+{
+    pthread_mutex_t 
+    pthread_cond_t
+    
+}thread_pool_t;
+void * worker(void* arg){
+    
+}
